e0bda38844539344a2181e52b613bc3f3ab94ff7
Xr4t0s <nicolastadros.maillol@gmail.com>
2025-11-16 01:33:36 +0100

    Added some tests

--- CPP_3/ex01/src/main.cpp
+++ CPP_3/ex01/src/main.cpp
@@ -1,5 +1,14 @@
 #include "ScavTrap.hpp"
 
 int main( void ) {
-	ScavTrap ct = ScavTrap("Foo");
+	ClapTrap ct = ClapTrap("FooClap");
+	ScavTrap st = ScavTrap("FooScav");
+
+	ct.attack("BarClap");
+	st.attack("BarScav");
+	ct.beRepaired(10);
+	st.beRepaired(10);
+	ct.takeDamage(10);
+	st.takeDamage(10);
+	st.guardGate();
 }
